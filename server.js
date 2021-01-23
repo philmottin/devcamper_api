acba440fc148d1e3ca2daa350a98ea7d5052f489
STRIX\Phil <philmottin@gmail.com>
2021-01-23 04:16:11 -0300

    Late commit

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const dotenv = require('dotenv');
+//const logger = require('./middleware/logger');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
-dotenv.config({ path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
+
+// MongoDB
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+//app.use(logger);
+
+// Dev loggib middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
+
+// Hangle unhandled promises rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});

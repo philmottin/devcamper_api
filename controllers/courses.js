cbff0e0b809d8e4103d175c2e8ab60be06f151f9
STRIX\Phil <philmottin@gmail.com>
2021-01-25 02:10:58 -0300

    This require was wrong AGAIN and unused. "const { QueryCursor } = require('mongoose');"

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,7 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
-const { QueryCursor } = require('mongoose');
+//const { QueryCursor } = require('mongoose');
 
 // @desc      Get courses
 // @route     GET /api/v1/courses

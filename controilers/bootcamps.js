9f2e9743a94d335a7f1492c1ae54d667f7a3569b
STRIX\Phil <philmottin@gmail.com>
2021-01-25 01:18:29 -0300

    This require was wrong ans unused. "const { Query } = require('mongoose');"

--- controilers/bootcamps.js
+++ controilers/bootcamps.js
@@ -2,7 +2,6 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
-const { Query } = require('mongoose');
 
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps

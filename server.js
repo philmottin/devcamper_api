4d74c241459d01700692f2a6747286c5f6a65f2d
STRIX\Phil <philmottin@gmail.com>
2021-01-26 06:33:53 -0300

    Added new User model created dummy route and controller auth for register method Installed packeges bcryptjs and jsonwebtoken

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connectDB();
 // Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auths = require('./routes/auth');
 
 const app = express();
 
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auths);
 
 // Must be after routers otherwise it won't catch bootcamps controller methods
 app.use(errorHandler);

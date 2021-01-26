3865fe82334f698e7f39443c59c727d69f074f12
STRIX\Phil <philmottin@gmail.com>
2021-01-26 01:18:26 -0300

    Calculating The Average Courses Cost per bootcamp with Aggregation Added pre and post middleware to run getAverageCost function Fix CourseSchema typo

--- models/Course.js
+++ models/Course.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const CourseShema = new mongoose.Schema({
+const CourseSchema = new mongoose.Schema({
   title: {
     type: String,
     trim: true,
@@ -38,4 +38,39 @@ const CourseShema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('Course', CourseShema);
+// Static method to get avg of course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log('Calculating avg cost...'.blue);
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: '$bootcamp',
+        averageCost: { $avg: '$tuition' },
+      },
+    },
+  ]);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.round(obj[0].averageCost),
+    });
+  } catch (err) {
+    console.error(err);
+  }
+  //console.log(obj);
+};
+
+// Call getAverageCost after save
+CourseSchema.post('save', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call getAverageCost before remove
+CourseSchema.pre('remove', function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+module.exports = mongoose.model('Course', CourseSchema);

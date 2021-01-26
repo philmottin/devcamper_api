242d55f7f09f9728eeef72ec7e5cf52ceaa0de4c
STRIX\Phil <philmottin@gmail.com>
2021-01-26 06:08:46 -0300

    Advanced Results Middleware for bootcamps and courses

--- routes/courses.js
+++ routes/courses.js
@@ -7,12 +7,24 @@ const {
   deleteCourse,
 } = require('../controllers/courses');
 
+const Course = require('../models/Course');
+const advancedResults = require('../middleware/advancedResults');
+
 // mergeParams:
 // Preserve the req.params values from the parent router.
 // If the parent and the child have conflicting param names, the child’s value take precedence.
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getCourses).post(addCourse);
+router
+  .route('/')
+  .get(
+    advancedResults(Course, {
+      path: 'bootcamp',
+      select: 'name description',
+    }),
+    getCourses
+  )
+  .post(addCourse);
 
 router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 

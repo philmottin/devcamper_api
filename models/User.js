82a0d028b9b3db1c1fd5f0eaaa2409d6520425a1
STRIX\Phil <philmottin@gmail.com>
2021-01-26 23:37:29 -0300

    Created method for genereating JWS token on User model Return JWS token when user is registered

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -39,5 +40,14 @@ UserSchema.pre('save', async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Sign JWT and return
+// methotd in scheme are used in instaciated objectes, so we have access to fields like _id below
+// if this was statics, witch are used directly from the Model without instaciating into an object, we wouldn't have acess to the fields
+UserSchema.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema);

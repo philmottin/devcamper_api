e0e6aa84511cdb256050263d9373864e3a3fdc4e
STRIX\Phil <philmottin@gmail.com>
2021-01-27 00:03:17 -0300

    Ímplemented login method, with server side validation for data coming from the request. Model method to compare password using bcrypt Login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,3 +20,33 @@ exports.register = asyncHandler(async (req, res, next) => {
   const token = user.getSignedJwtToken();
   res.status(200).json({ success: true, token });
 });
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(
+      new ErrorResponse('Please provide an email and password.', 400)
+    );
+  }
+
+  //check for user
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) {
+    return next(new ErrorResponse('Invalid crdedentials.', 401));
+  }
+
+  // check if password matches
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid crdedentials.', 401));
+  }
+
+  // Create token
+  const token = user.getSignedJwtToken();
+  res.status(200).json({ success: true, token });
+});

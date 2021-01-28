152652b5e802f47cdc79911ce8103617ffa00a83
Phil Mottin <philmotin@gmail.com>
2021-01-28 02:25:00 -0300

    Added method getResetPasswordToken on User model to generate and hash password token Update UserSchema.pre on model to allow method forgotPassword in auth controller to run, otherwise it gets called with empty password and throw an error Add forgotPassword on auth controller and route.

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,6 +47,33 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc      Get current logged user
+// @route     POST /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  //req.user comes from auth middleware
+  res.status(200).json({ success: true, data: req.user });
+});
+
+// @desc      Forgot password
+// @route     POST /api/v1/auth/forgotpassword
+// @access    Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  const resetToken = user.getResetPasswordToken();
+  //console.log(resetToken);
+  await user.save({
+    validateBeforeSave: false,
+  });
+
+  res.status(200).json({ success: true, data: user });
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
@@ -70,11 +97,3 @@ const sendTokenResponse = (user, statusCode, res) => {
     .cookie('token', token, options)
     .json({ success: true, token });
 };
-
-// @desc      Get current logged user
-// @route     POST /api/v1/auth/me
-// @access    Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  //req.user comes from auth middleware
-  res.status(200).json({ success: true, data: req.user });
-});

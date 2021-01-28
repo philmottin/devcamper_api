08b9c9b22cc1b6c31324fb933236efb33079427e
Phil Mottin <philmotin@gmail.com>
2021-01-28 20:07:50 -0300

    Add get single Review and seeder for reviews
    Add getReview method on reviews controller
    Add getReview route
    Fiz error code outside parenthesis on courses controller

--- seeder.js
+++ seeder.js
@@ -12,6 +12,7 @@ dotenv.config({
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -34,12 +35,17 @@ const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, 'utf8')
 );
 
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf8')
+);
+
 // Import unto DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
     console.log('Data imported'.green.inverse);
     process.exit();
   } catch (err) {
@@ -53,6 +59,7 @@ const deleteData = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data destroyed'.red.inverse);
     process.exit();
   } catch (err) {

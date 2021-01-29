8fb570b08d7a1ccbed175ec80e0c68da014d24f0
Phil Mottin <philmotin@gmail.com>
2021-01-28 23:32:33 -0300

    Add API limit and prevent HTTP polution
    Installed express-rate-limit and hpp packages.
    Use them as middleware in server.js.
    Set global limit to 100 request in 10 minutes.

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 var xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+var hpp = require('hpp');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -50,8 +52,17 @@ app.use(mongoSanitize());
 app.use(helmet());
 
 // Prevent XSS attacks
-
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10 minutes
+  max: 2,
+});
+app.use(limiter);
+
+// Prevent http polution
+app.use(hpp());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));

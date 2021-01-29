3443c4a02ed3ac2c56ee22419bbc5f0e162eab9f
Phil Mottin <philmotin@gmail.com>
2021-01-28 22:56:04 -0300

    Prevent noSQL injection
    Installed express-mongo-sanitize package
    Used ṕackage as middleware in server.js
    Fix error message in error middleware

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const colors = require('colors');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -39,6 +40,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // File uploading
 app.use(fileUpload());
+
+// Sanitize data
+app.use(mongoSanitize());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));

cbf95c52f455989940c62865c042a7f09f1a3faf
Phil Mottin <philmotin@gmail.com>
2021-01-28 22:48:16 -0300

    Ignore use of  cookies
    Fix description of logout method in auth controller

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,10 +14,11 @@ exports.protect = asyncHandler(async (req, res, next) => {
   ) {
     // Set token from Bearer authentication
     token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies.token) {
-    // Set token from cookie
-    token = req.cookies.token;
   }
+  //else if (req.cookies.token) {
+  // Set token from cookie
+  //   token = req.cookies.token;
+  // }
 
   // Make sure token exist
   if (!token) {

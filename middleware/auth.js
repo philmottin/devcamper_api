3b437d06c4ee5e9049754cc3a17b7c6fc5fa6f60
Phil Mottin <philmotin@gmail.com>
2021-01-28 21:48:43 -0300

    Add logout functionality and clear cookies
    Add logout method in auth controller
    Clear cookies in auth middleware
    Add logout route
    Fix some authorized typos

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,15 +12,16 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // Set token from Bearer authentication
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.token) {
+    // Set token from cookie
+    token = req.cookies.token;
   }
-  //else if (req.cookies.token) {
-  //  token = req.cookies.token;
-  //}
 
   // Make sure token exist
   if (!token) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 
   try {
@@ -31,7 +32,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.user = await User.findById(decoded.id);
     next();
   } catch (err) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
 
@@ -41,7 +42,7 @@ exports.authorize = (...roles) => {
     if (!roles.includes(req.user.role)) {
       return next(
         new ErrorResponse(
-          `User role ${req.user.role} is not authorize to access this route`,
+          `User role ${req.user.role} is not authorized to access this route`,
           403
         )
       );
